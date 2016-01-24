49ee979a78b64845a08373d89f69f46d79a686bf
flas1 <flasum@yahoo.co.uk>
2016-01-24 11:32:50 +0000

    Initial solutioncreated. Unit test for prime number checker

---
+++ CTM.WordCounter/CTM.WordCountCalculator.Test/PrimeNumberCheckerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using CTM.WordCounter;
+
+namespace CTM.WordCountCalculator.Test
+{
+    [TestFixture]
+    public class PrimeNumberCheckerTests
+    {
+        [Test]
+        public void IsPrimeReturnsTrueForPrimeNumbersTest([Values(2, 3, 5,7, 5279, 67867979, 982451653)]int numberToCheck)
+        {
+            var checker = new PrimeNumberChecker();
+            Assert.IsTrue(checker.IsPrime(numberToCheck));
+        }
+
+        [Test]
+        public void IsPrimeReturnsFalseForNonPrimeNumbersTest([Values(-1, 0, 1, 4, 15, 21, 30, 333, 34535343)]int numberToCheck)
+        {
+            var checker = new PrimeNumberChecker();
+            Assert.IsTrue(checker.IsPrime(numberToCheck));
+        }
+    }
+}
---
+++ CTM.WordCounter/CTM.WordCounter/PrimeNumberChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTM.WordCounter
+{
+    public class PrimeNumberChecker : IPrimeNumberChecker
+    {
+        public bool IsPrime(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
